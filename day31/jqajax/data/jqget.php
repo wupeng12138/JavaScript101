60eb16745f5dc0f05097de7e1f8f8562dcafaa2e
wupeng12138 <40687045+wupeng12138@users.noreply.github.com>
2018-11-12 23:51:40 +0800

    Add files via upload

---
+++ day31/jqajax/data/jqget.php
@@ -0,0 +1,25 @@
+﻿<?php
+	
+	$head = "<ul>
+				<li>link1</li>
+				<li>link2</li>
+				<li>link3</li>
+				<li>link4</li>
+			</ul>";
+	$foot = "<span>底部</span>";
+	
+	$c = '[{"name":"adin"},{"name":"liyang"}]';
+	
+	
+	$type = @$_GET["type"];
+	
+//	if($type == "header"){
+//		echo $head;
+//	}else if($type == "footer"){
+//		echo $foot;
+//	}else{
+//		echo "没有数据";
+//	}
+	
+	echo $c;
+?>
